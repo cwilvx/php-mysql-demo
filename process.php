e2c06a86ccbe01af3d830f6d325d831e72ca805d
geoffrey45 <geoffreymungai45@gmail.com>
2021-01-06 13:01:22 +0300

    first commit

---
+++ process.php
@@ -0,0 +1,23 @@
+<?php include 'database.php'; ?>
+
+<?php
+
+// create a variable
+$first_name=$_POST['first_name'];
+$last_name=$_POST['last_name'];
+$department=$_POST['department'];
+$email=$_POST['email'];
+
+//Execute the query
+
+
+mysqli_query($connect,"INSERT INTO employees1 (first_name,last_name,department,email)
+		        VALUES ('$first_name','$last_name','$department','$email')");
+				
+	if(mysqli_affected_rows($connect) > 0){
+	echo "<p>Employee Added</p>";
+	echo "<a href='/php/index.php'>Go Back</a>";
+} else {
+	echo "Employee NOT Added<br />";
+	echo mysqli_error ($connect);
+}
